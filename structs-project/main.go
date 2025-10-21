807521543c8d4b4bcab4241560e2436be4362f70
Sahadat20 <shahadathossainroni941@gmail.com>
2025-10-21 17:45:39 +0600

    Learning interface

--- structs-project/main.go
+++ structs-project/main.go
@@ -7,22 +7,72 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+// type displayer interface{
+// 	Display()
+// }
+
+type outputable interface {
+	saver
+	Display()
+}
+
+// type outputable interface{
+// 	Save() error
+// 	Display()
+// }
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text")
+
+	todo, err := todo.New(todoText)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = outputData(todo)
+
+	// todo.Display()
+	// err = saveData(todo)
+	if err != nil {
+		return
+	}
 	userNote, err := note.New(title, content)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(userNote)
+	// userNote.Display()
+	// err = saveData(userNote)
 	if err != nil {
-		fmt.Println("Saving failed")
 		return
 	}
+
+}
+
+func outputData(data outputable) error {
+	data.Display()
+	return saveData(data)
+
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println("Saving failed")
+		return err
+	}
 	fmt.Println("Save success")
+	return nil
+
 }
 
 func getNoteData() (string, string) {
